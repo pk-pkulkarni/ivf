ecf5ae2fda4349750857913d9654fd130236d89d
pk-pkulkarni <prasadkulkarni932@gmail.com>
2017-10-27 16:09:30 +0530

    Add User - Service

--- services/config.php
+++ services/config.php
@@ -1,11 +1,21 @@
 <?php
-
+$servername = "127.0.0.1"; //Ip or HostName(localshost)
+$username = "root"; //Database Username
+$password = ""; //Database Pwd
+$dbname = "ivf"; //Database Name
 // Create connection
-$conn = mysqli_connect('localhost', 'root', '', 'angular_test');
+$conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+/*if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully";
+$sql = 'SELECT * from role';
+$rows = array();
+$result = $conn->query($sql);
+while($r = mysqli_fetch_assoc($result)) {
+    $rows[] = $r;
 }
-
+echo json_encode($rows);*/
 ?>
---
+++ services/user/add.php
@@ -0,0 +1,24 @@
+<?php
+include ("../config.php");
+
+//mysqli_real_escape_string =  Escapes special characters in a string for use in an SQL statement.
+
+$firstname = mysqli_real_escape_string($conn, $_REQUEST['firstname']);
+$lastname = mysqli_real_escape_string($conn, $_REQUEST['lastname']);
+$email = $_REQUEST['email'];
+$contact =  $_REQUEST['contact'];
+$password = $_REQUEST['password'];
+$center_id = $_REQUEST['center_id'];
+$role_id = $_REQUEST['role_id'];
+
+$result = array();
+
+$sql = "insert into user(firstname,lastname,email,contact,password,center_id,role_id) values ('$firstname','$lastname','$email','$contact','$password','$center_id','$role_id')";
+
+if(mysqli_query($conn, $sql)){
+    $result['success'] = "Record is Created Successfully";
+} else{
+	$result['error'] = "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+
+echo json_encode($result);
