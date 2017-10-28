0edeac1856e465eb9de5baf9bf86137f44ef83aa
pk-pkulkarni <prasadkulkarni932@gmail.com>
2017-10-28 15:58:01 +0530

    Get service is added for USers and Center

---
+++ services/user/get.php
@@ -0,0 +1,16 @@
+<?php
+include ("../config.php");
+include ("../common.php");
+
+//mysqli_real_escape_string =  Escapes special characters in a string for use in an SQL statement.
+
+$result = array();
+
+$sql = "select * from user";
+
+$rows = $conn->query($sql);
+while($r = mysqli_fetch_assoc($rows)) {
+    $result[] = $r;
+}
+
+echo json_encode($result);
