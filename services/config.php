d2e5d153a0475c56fbced5e6a5f72892c12d5b8a
pk-pkulkarni <prasadkulkarni932@gmail.com>
2017-12-18 16:23:40 +0530

    Trying to connect milesweb(cloud) database

--- services/config.php
+++ services/config.php
@@ -1,8 +1,13 @@
 <?php
 
-$servername = "127.0.0.1"; //Ip or HostName(localshost)
+/*$servername = "127.0.0.1"; //Ip or HostName(localshost)
 $username = "root"; //Database Username
-$password = ""; //Database Pwd
+$password = ""; //Database Pwd*/
+
+$servername = "node3309-env-5653122.mj.milesweb.cloud"; //Ip or HostName(localshost)
+$username = "root"; //Database Username
+$password = "OYKscy21368"; //Database Pwd
+
 $dbname = "ivf"; //Database Name
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
