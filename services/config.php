6e12805a2e07654cfffbd8bee67b5954aa59f3ca
pk-pkulkarni <prasadkulkarni932@gmail.com>
2017-12-19 11:32:14 +0530

    reverted back config changes made for cloud

--- services/config.php
+++ services/config.php
@@ -1,13 +1,11 @@
 <?php
-
-/*$servername = "127.0.0.1"; //Ip or HostName(localshost)
+/*$servername = "node3309-env-5653122.mj.milesweb.cloud"; //Ip or HostName(localshost)
 $username = "root"; //Database Username
-$password = ""; //Database Pwd*/
+$password = "OYKscy21368"; //Database Pwd*/
 
-$servername = "node3309-env-5653122.mj.milesweb.cloud"; //Ip or HostName(localshost)
+$servername = "127.0.0.1"; //Ip or HostName(localshost)
 $username = "root"; //Database Username
-$password = "OYKscy21368"; //Database Pwd
-
+$password = ""; //Database Pwd
 $dbname = "ivf"; //Database Name
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
